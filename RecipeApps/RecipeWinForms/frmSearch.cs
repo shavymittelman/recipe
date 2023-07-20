6d448cb2b6ae9410a27539bf749383ca08703afd
shavy <shavymtl@gmail.com>
2023-07-20 13:40:50 -0400

    frm search update

--- RecipeApps/RecipeWinForms/frmSearch.cs
+++ RecipeApps/RecipeWinForms/frmSearch.cs
@@ -1,13 +1,5 @@
 ﻿using CPUFramework;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace RecipeWinForms
 {
@@ -17,14 +9,39 @@ namespace RecipeWinForms
         {
             InitializeComponent();
             btnSearch.Click += BtnSearch_Click;
+            gRecipe.CellDoubleClick += GRecipe_CellDoubleClick;
+            FormatGrid();
         }
 
-        private void BtnSearch_Click(object? sender, EventArgs e)
+        private void SearchRecipe()
         {
-            string sql = "select RecipeName from Recipe r where r.RecipeName like '%" + txtSearch.Text + "%'";
+            string sql = "select RecipeId, RecipeName from Recipe r where r.RecipeName like '%" + txtSearch.Text + "%'";
             DataTable dt = SQLUtility.GetDataTable(sql);
             gRecipe.DataSource = dt;
+            gRecipe.Columns["RecipeId"].Visible = false;
+        }
 
+        private void ShowRecipeForm(int rowindex)
+        {
+            int id = (int)gRecipe.Rows[rowindex].Cells["RecipeId"].Value;
+            frmRecipe frm = new frmRecipe();
+            frm.ShowForm(id);
+        }
+        private void FormatGrid()
+        {
+            gRecipe.AllowUserToAddRows = false;
+            gRecipe.ReadOnly = true;
+            gRecipe.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            gRecipe.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        private void BtnSearch_Click(object? sender, EventArgs e)
+        {
+            SearchRecipe();
+        }
+
+        private void GRecipe_CellDoubleClick(object? sender, DataGridViewCellEventArgs e)
+        {
+            ShowRecipeForm(e.RowIndex);
         }
     }
 }
